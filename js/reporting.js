9eb3b70d4b483facddbda4bc04d51a17eb42586e
Shuki Shukrun <shukrun.shuki@gmail.com>
2014-08-25 16:49:47 +0300

    report

--- js/reporting.js
+++ js/reporting.js
@@ -13,8 +13,9 @@ $(function(){
 			navButtonAdd: false,
 		},
 		beforeImport: function(params){
-			var grid = $(this);
-			//params['_fields_'].push({name: 'id_number', value: 6666});
+			//var grid = $(this);
+			if(fn_reporting.parent_id)
+				params['_fields_'].push({name: 'parent', value: fn_reporting.parent_id});
 		},
 		copy:{
 			navButtonAdd: false
@@ -66,6 +67,7 @@ $(function(){
 
 	$.jset.fn.registerGridDefinition('reporting', {
 	  	source: 'reporting', 
+  		item_name: 'דיווח ממשרד הרווחה',
 		load_edit_record: false,
 		template: {
 			use: true,
@@ -93,18 +95,50 @@ $(function(){
 		columnChooser:{
 			navButtonAdd: true
 		},
+		loadComplete: function(data){
+/*			var grid = $('table#reporting_item');
+			if(fn_reporting.parent_id){
+				fn_reporting.parent_id = false;
+				grid[0].triggerToolbar();
+			}*/
+		},
+		onInitializeForm: function(formid){			
+			$($.jset.fn.get_form_field(formid, 'date')).on('change', function(){
+				var value = $(this).val();
+				if(value){
+					var parts = value.split('/');
+					$($.jset.fn.get_form_field(formid, 'month')).val(parts[1].replace(/^0+/, ''));
+					$($.jset.fn.get_form_field(formid, 'year')).val(parts[2]);
+				}
+				else
+				{
+					$($.jset.fn.get_form_field(formid, 'month')).val('');
+					$($.jset.fn.get_form_field(formid, 'year')).val('');
+				}
+			});
+		},
 		beforeShowForm: function(formid){
+			var grid = $(this);
+			if(grid.data('form_action') == 'add')
+				$($.jset.fn.get_form_field(formid, 'date')).trigger('change');			
 		},
 		loadCompleteInit: function(data){
 		},
 		beforeSubmit: function(postdata, formid){
+			if(postdata.file == undefined)
+				return [true];
+				
 			var grid = $(this);
 			var savedData = grid.jqGrid('getFormSavedData');
 			fn_reporting.recreate_items = (postdata.file && savedData.file != postdata.file);
 			fn_reporting.reload_grid = savedData.file != postdata.file;
 			return [true];
 		},
-		afterSubmit: function(response, postdata, frmoper){
+		afterSubmit: function(response, postdata, frmoper, obj){
+			if(frmoper === undefined)
+				return [true];
+							
+			fn_reporting.parent_id = frmoper == 'add' ? obj.id : false;			
 			var grid = $('table#reporting_item');
 			if(fn_reporting.recreate_items){
 				var filename = postdata['file'].split('/')[1];
@@ -115,7 +149,11 @@ $(function(){
 					}
 					
 					if(data > 0)
-						grid[0].triggerToolbar();
+						setTimeout (function(){ 
+					       grid[0].triggerToolbar();
+					    },0);
+
+						//grid[0].triggerToolbar();
 					alert(data + ' ' + $.jset.messages.recordsAdded);
 				});
 			}
@@ -123,6 +161,17 @@ $(function(){
 				grid[0].triggerToolbar();
 		
 			return [true];
+		},
+		afterSubmitError: function(response, postdata, frmoper, obj){
+			var message = obj.error.message;
+			switch(obj.error.info[1]){
+				case 1062: 
+					message = 'קיים כבר דיווח לחודש זה.';
+					break;
+				default:
+					;
+			}
+			return [false, message];
 		},
 		beforeRequest: function(){
 		},
@@ -151,5 +200,11 @@ $(function(){
 
 var fn_reporting= {
 	recreate_items: false,
-	reload_grid: false
+	reload_grid: false,
+	parent_id: false
 };
+
+/*function pad(str, max) {
+  str = str.toString();
+  return str.length < max ? pad("0" + str, max) : str;
+}*/
