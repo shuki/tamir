3b6824a6ea22ccb1fe170d89649dbd570611c496
Shuki Shukrun <shukrun.shuki@gmail.com>
2018-01-22 10:56:36 +1300

    jset.php handle export.

--- jset/server/jset.php
+++ jset/server/jset.php
@@ -2,18 +2,17 @@
 include('autoload.php');
 
 $request = jset_dispatch::get_real_POST_GET();
-if(defined('config::version') && config::version != $request['_version_']){
-	$version = config::version;
-	$result->error->type = "version";
-	$result->error->message = "version";
-	echo json_encode($result);
-	die;
-}
-
 $result = jset_dispatch::get($request);
 
 if($request['_methods_'] == 'export'){
 	header('Content-disposition: attachment; filename=' . $request['_source_'] . '.csv');
+	header('Content-type: text/csv');
+	echo substr($result, 0, -2);
+	exit;
+}
+
+if($request['_file_output_']){
+	header('Content-disposition: attachment; filename=file.txt');
 	header('Content-type: text/csv');
 }
 
