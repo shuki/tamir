a796ffd47574f590de339d95f4c8fbfdbd4b289e
Shuki Shukrun <shukrun.shuki@gmail.com>
2019-07-18 01:36:50 +1200

    .htaccess for getting file from archive if does not exist in files folder.

---
+++ jset/server/get_file.php
@@ -0,0 +1,19 @@
+<?php 
+include('autoload.php');
+const default_local_files_directory = '../../files/';
+
+if(defined('config::local_files_directory') && defined('config::remote_files_directory'))
+	$file_url = file_exists(config::local_files_directory . $_GET['filename']) ? config::local_files_directory . $_GET['filename'] : (file_exists(config::remote_files_directory . $_GET['filename']) ? config::remote_files_directory . $_GET['filename'] : false );
+else
+	$file_url = file_exists(default_local_files_directory . $_GET['filename']) ? default_local_files_directory . $_GET['filename'] : false;
+
+if(!$file_url){
+	header($_SERVER["SERVER_PROTOCOL"]." 404 Not Found", true, 404);
+	echo 'file not found';
+	exit;
+}
+
+$mime_content_type = mime_content_type($file_url);
+header("Content-type: $mime_content_type");
+header("Content-disposition: inline; filename=\"" . basename($file_url) . "\"");
+readfile($file_url);
