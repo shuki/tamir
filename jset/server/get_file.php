ddf7dc9ac005d492787cb9c889a9672904feb254
Shuki Shukrun <shukrun.shuki@gmail.com>
2020-03-25 01:08:46 +1300

    get_file

--- jset/server/get_file.php
+++ jset/server/get_file.php
@@ -1,8 +1,9 @@
 <?php 
 include('autoload.php');
-jset_login::verify('../');
-
 const default_local_files_directory = '../../files/';
+const files_relative_to_root = '../';
+
+jset_login::verify(files_relative_to_root);
 
 if(defined('config::local_files_directory') && defined('config::remote_files_directory'))
 	$file_url = file_exists(config::local_files_directory . $_GET['filename']) ? config::local_files_directory . $_GET['filename'] : (file_exists(config::remote_files_directory . $_GET['filename']) ? config::remote_files_directory . $_GET['filename'] : false );
--- misc/db_changes.sql
+++ misc/db_changes.sql
@@ -2,3 +2,4 @@
 ALTER TABLE `jset_table`
 	ADD COLUMN `export_source` TEXT NULL DEFAULT NULL AFTER `source`,
 	ADD COLUMN `comments` TEXT NULL DEFAULT NULL AFTER `system`;
+
