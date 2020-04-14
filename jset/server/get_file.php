86aabb7847c1b1146c5a6ad0588f6ed99c1f8632
Shuki Shukrun <shukrun.shuki@gmail.com>
2020-04-14 13:09:09 +1200

    get file login by config

--- jset/server/get_file.php
+++ jset/server/get_file.php
@@ -3,7 +3,8 @@ include('autoload.php');
 const default_local_files_directory = '../../files/';
 const files_relative_to_root = '../';
 
-jset_login::verify(files_relative_to_root);
+if(defined('config::login') && config::login)
+	jset_login::verify(files_relative_to_root);
 
 if(defined('config::local_files_directory') && defined('config::remote_files_directory'))
 	$file_url = file_exists(config::local_files_directory . $_GET['filename']) ? config::local_files_directory . $_GET['filename'] : (file_exists(config::remote_files_directory . $_GET['filename']) ? config::remote_files_directory . $_GET['filename'] : false );
